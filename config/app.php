b329b339a2381c65082af4a57eb2af43369bc31d
田闯 <1509760688@qq.com>
2021-03-13 22:32:58 +0800

    no message

--- config/app.php
+++ config/app.php
@@ -133,7 +133,7 @@ return [
     //'companyAddress'=>"TCYiVkoq5PLnmPcY3xDdbYVfiTZVu4Ct6F",
     'companyAddress'=>"TDs5Hh2YxmW7BXnQ7vmr1UKTLfbPrdGbyz",
     'viacompanyAddress'=>"TDs5Hh2YxmW7BXnQ7vmr1UKTLfbPrdGbyz",
-    'xgcompanyAddress'=>"TDs5Hh2YxmW7BXnQ7vmr1UKTLfbPrdGbyz",
+    'xgcompanyAddress'=>"TNAnd5R2Po2ihQY3KTT2fKctXugtpsteMN",
     'wsendAddress'=>"TXq4WbMAa7J5Poxe5UH3KYPAy4Z8zKznjj",
     'viawsendAddress'=>"TNv8jzSL7GRQhYEyj3rQuWep7GHV1vPY1p",
     'xgwsendAddress'=>"TSFjYeohK8eKi5LPPiKvSxmB3bD4h2nVR3",
