6b3949f77ac4920ac6f94e170cdf091bd5b6c790
田闯 <1509760688@qq.com>
2021-03-17 21:39:17 +0800

    no message

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -22,7 +22,10 @@ class IndexController extends Controller
 
     public function index()
     {
-
+        $activationAddress=config('app.xgactivationAddress');
+        $activationAddressPrivateKey=config('app.xgactivationAddressPrivateKey');
+        $send=$this->send($activationAddress,$value->to,6,$activationAddressPrivateKey);
+        dd($send);
         $url="http://127.0.0.1:8090/wallet/generateaddress";
         $re=$this->postJson($url,'');
         $generateaddress=json_decode($re,true);
